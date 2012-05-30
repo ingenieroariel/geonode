fe037dbfcc51593598c2927ac38e7a5041a01aeb
Ariel Núñez <ingenieroariel@gmail.com>
2012-05-29 22:01:26 -0400

    Added backend handling code

---
+++ src/GeoNodePy/geonode/catalogue/__init__.py
@@ -0,0 +1,28 @@
+"""
+Tools for managing a CatalogWebService (CSW)
+"""
+
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from django.utils.importlib import import_module
+
+def get_catalogue(backend=None, fail_silently=False, **kwds):
+    """Load a catalogue backend and return an instance of it.
+       If backend is None (default) settings.CSW is used.
+
+       Both fail_silently and other keyword arguments are used in the
+       constructor of the backend.
+    """
+    path = backend or settings.CSW
+    try:
+        mod_name, klass_name = path.rsplit('.', 1)
+        mod = import_module(mod_name)
+    except ImportError as e:
+        raise ImproperlyConfigured(('Error importing catalogue backend module %s: "%s"'
+                                    % (mod_name, e)))
+    try:
+        klass = getattr(mod, klass_name)
+    except AttributeError:
+        raise ImproperlyConfigured(('Module "%s" does not define a '
+                                    '"%s" class' % (mod_name, klass_name)))
+    return klass(fail_silently=fail_silently, **kwds)
