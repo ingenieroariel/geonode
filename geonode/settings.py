873092a28d209f457a38aec6eca9acabb32086f0
Ariel Núñez <ingenieroariel@gmail.com>
2012-09-19 11:45:40 -0400

    Added InaSAFE and geonode_safe log handlers

--- geonode/settings.py
+++ geonode/settings.py
@@ -197,7 +197,11 @@ LOGGING = {
             "handlers": ["console"],
             "level": "ERROR",
         },
-        "safe": {
+        "InaSAFE": {
+            "handlers": ["console"],
+            "level": "ERROR",
+        },
+        "geonode_safe": {
             "handlers": ["console"],
             "level": "ERROR",
         },
