3ea7ef30517a9b1160e97808e548a3614412ca50
Ariel Nunez <ingenieroariel@gmail.com>
2014-05-07 12:55:52 +0200

    Added django-leaflet, django-geojson and django-shapes

--- setup.py
+++ setup.py
@@ -105,6 +105,9 @@ setup(name='GeoNode',
         "django-downloadview==1.2",
         "django-tastypie==0.11.0",
         "django-polymorphic==0.5.3",
+        "django-leaflet==0.13.2",
+        "django-geojson==2.4.0",
+        "django-shapes==0.2.0",
         #catalogue
         "pycsw==1.8.0",
         # setup
